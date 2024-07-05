d06e5f5c2b7a05a43622a1c518ced65ceee303a1
MonteiroVideo <166059439+MonteiroVideo@users.noreply.github.com>
2024-07-04 21:06:16 -0300

    Update presets.js

--- presets.js
+++ presets.js
@@ -164,8 +164,6 @@ export function getPresetDefinitions() {
 
     };
 
-
-    // Função auxiliar para adicionar presets SET
     function addSetPreset(minutes) {
         presets[`preset_set${minutes}`] = {
             type: 'button',
@@ -193,8 +191,7 @@ export function getPresetDefinitions() {
             ],
         };
     }
-
-    // Adicionar presets SET
+    
     addSetPreset(1);
     addSetPreset(5);
     addSetPreset(15);
@@ -215,7 +212,6 @@ export function getPresetDefinitions() {
     addTimePreset('AA', 'AA', 'AA');
     addTimePreset('aa', 'aa', 'aa');
 
-    // Função auxiliar para adicionar presets de tempo
     function addTimePreset(actionId, text, name) {
         presets[`preset_${actionId}`] = {
             type: 'button',
@@ -240,6 +236,37 @@ export function getPresetDefinitions() {
             ],
         };
     }
+
+for (let i = 1; i <= 20; i++) {
+    const pptCommand = i < 10 ? `preset\n0${i}` : `preset\n${i}`
+    const pptText = `preset${i}`
+    const variableName = `$(Cronos:item${i})`; // Nome da variável correspondente ao item
+    presets[`preset_${pptCommand}`] = {
+        type: 'button',
+        category: 'Preset',
+        name: `Send ${pptCommand.toUpperCase()} Command`,
+        style: {
+            text: variableName,
+            size: '14',
+            color: '16777215',
+            bgcolor: '25600',
+            show_topbar: false
+        },
+        steps: [
+            {
+                down: [
+                    {
+                        actionId: 'send',
+                        options: {
+                            id_send: pptText,
+                        },
+                    },
+                ],
+                up: [],
+            },
+        ],
+    }
+}
 
 
     return presets;
